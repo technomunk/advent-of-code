cbe8b40db8741f163f521e0a22629eb17a0f85ff
Griffone <thegriffones@gmail.com>
2021-12-03 00:38:56 +0100

    Adhere to Ruby style

--- 2021/01_sonar_sweep.rb
+++ 2021/01_sonar_sweep.rb
@@ -1,6 +1,6 @@
-def solve_part1 lines
+def solve_part1(lines)
 	increments = 0
-	depths = lines.map{|el| el.to_i}
+	depths = lines.map { |el| el.to_i }
 	for a, b in depths.each_cons(2)
 		if b > a
 			increments += 1
@@ -9,11 +9,11 @@ def solve_part1 lines
 	return increments
 end
 
-def solve_part2 lines
+def solve_part2(lines)
 	increments = 0
-	depths = lines.map{|el| el.to_i}
+	depths = lines.map { |el| el.to_i }
 	triplets = depths.each_cons(3)
-	triplet_sums = triplets.map{|t| t.sum}
+	triplet_sums = triplets.map { |t| t.sum }
 	for a, b in triplet_sums.each_cons(2)
 		if b > a
 			increments += 1
@@ -23,5 +23,5 @@ def solve_part2 lines
 end
 
 lines = IO.readlines("01.txt")
-puts solve_part1(lines)
-puts solve_part2(lines)
+puts(solve_part1(lines))
+puts(solve_part2(lines))
--- 2021/02_dive.rb
+++ 2021/02_dive.rb
@@ -1,4 +1,4 @@
-def solve_part1 lines
+def solve_part1(lines)
 	x, y = 0, 0
 	for line in lines do
 		dir, amt = line.split
@@ -17,7 +17,7 @@ def solve_part1 lines
 	return x * y
 end
 
-def solve_part2 lines
+def solve_part2(lines)
 	x, y, dy = 0, 0, 0
 
 	for line in lines do
@@ -40,5 +40,5 @@ end
 
 lines = IO.readlines("02.txt")
 
-puts solve_part1(lines)
-puts solve_part2(lines)
+puts(solve_part1(lines))
+puts(solve_part2(lines))
