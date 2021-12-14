77fd367cbe583daaa0fe37be7060334238a36888
technomunk <grigory.glukhov@billogram.com>
2021-12-14 13:14:53 +0100

    Solve part 2

--- 2021/13_extended_polymerization.rb
+++ 2021/13_extended_polymerization.rb
@@ -1,45 +1,66 @@
-class Array
+class Hash
 	def apply(rules)
-		inserted = self.each_cons(2)
-			.map { |p| rules[p] }
-		self.zip(inserted)
-			.flatten
-			.compact
-	end
+		result = Hash.new
+		result.default = 0
+		for pair, count in self do
+			b = rules[pair]
+			if b
+				a, c = pair.chars
+				ab = a + b
+				bc = b + c
 
-	def apply!(rules)
-		inserted = self.each_cons(2)
-			.map { |p| rules[p] }
-
-		offset = 1
-		for e, i in inserted.each_with_index do
-			if e
-				self.insert(i + offset, e)
-				offset += 1
+				result[ab] += count
+				result[bc] += count
+			else
+				result[pair] = count
 			end
 		end
+		return result
+	end
+
+	def apply_n(rules, n)
+		result = self
+		for _ in 1..n do
+			result = result.apply(rules)
+		end
+		return result
+	end
+
+	def count_first_elements
+		elements = Hash.new
+		elements.default = 0
+
+		for pair, count in self do
+			a, _ = pair.chars
+			elements[a] += count
+		end
+
+		return elements
+	end
+
+	def solve(rules, n, last_element)
+		elements = self.apply_n(rules, n).count_first_elements
+		elements[last_element] += 1
+		_, min, _, max = elements.minmax_by { |_, v| v }
+			.flatten
+		return max - min
 	end
 end
 
-def solve(chain, rules, n)
-	for i in 1..n do
-		chain.apply!(rules)
-	end
-	_, min, _, max = chain.tally
-		.minmax_by { |_, v| v }
-		.flatten
-	return max - min
-end
-
-lines = STDIN.readlines
+chain, _, *rules = STDIN.readlines
 	.map { |l| l.strip }
 
-chain, rules = lines[0], lines[2..]
-
-chain = chain.chars.to_a
-rules = rules.map { |r| r.split(" -> ") }
-	.map { |p, e| [p.chars.to_a, e] }
+rules = rules.map { |l| l.split(" -> ") }
 	.to_h
 
-puts(solve(chain, rules, 10))
-puts(solve(chain, rules, 30))
+pairs = chain.chars
+	.each_cons(2)
+	.map { |a, b| a + b }
+	.tally
+
+last_element = chain.chars.last
+part1 = pairs.solve(rules, 10, last_element)
+part2 = pairs.solve(rules, 40, last_element)
+
+puts(part1)
+puts(part2)
