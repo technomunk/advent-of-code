e19a3f58e7f1e89ff92f220beae2f0a7d63a1847
technomunk <thegriffones@gmail.com>
2022-12-15 22:46:19 +0100

    Clean up and include part 1

--- 2022/15_beacon_exclusion_zone.py
+++ 2022/15_beacon_exclusion_zone.py
@@ -1,6 +1,6 @@
+import sys
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from multiprocessing import cpu_count
-import sys
 
 from utils import match_transform_inputs
 
@@ -19,12 +19,27 @@ def check_range(y_range: range, x_range: range, beacons: list[tuple[Point, int]]
     return None
 
 
+def check_row(y: int, coords: list[tuple[Point, Point]]) -> int:
+    overlaps = filter(None, (beacon_overlap(y, beacon, manhattan(beacon, scanner)) for beacon, scanner in coords))
+    # I'm too lazy to do pure ranges
+    row = set()
+    for start, stop in overlaps:
+        for value in range(start, stop + 1):
+            row.add(value)
+
+    for _, scanner in coords:
+        if scanner[1] == y:
+            row.discard(scanner[0])
+
+    return len(row)
+
+
 def solve_puzzle(coords: list[tuple[Point, Point]]) -> None:
     beacons = list(map(lambda x: (x[0], manhattan(*x)), coords))
     max_coord = 4_000_000 if len(sys.argv) == 1 else 20
-    checked_row = 2_000_000 if len(sys.argv) == 1 else 10
     thread_count = cpu_count()
     range_height = max_coord // thread_count
+    print(check_row(2_000_000 if len(sys.argv) == 1 else 10, coords))
     with ThreadPoolExecutor(thread_count) as executor:
         futures = {
             executor.submit(
@@ -46,6 +61,13 @@ def point(x: str, y: str) -> Point:
 
 def manhattan(a: Point, b: Point) -> int:
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+def beacon_overlap(y: int, beacon: Point, radius: int) -> tuple[int, int] | None:
+    width = radius - abs(y - beacon[1])
+    if width < 0:
+        return None
+    return beacon[0] - width, beacon[0] + width
 
 
 if __name__ == "__main__":
