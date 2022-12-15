21794aeed36d69d02763c4cfc284d24e31030c12
technomunk <thegriffones@gmail.com>
2022-12-15 22:30:18 +0100

    Casual part 2

--- 2022/15_beacon_exclusion_zone.py
+++ 2022/15_beacon_exclusion_zone.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from multiprocessing import cpu_count
 import sys
 
 from utils import match_transform_inputs
@@ -5,15 +7,37 @@ from utils import match_transform_inputs
 Point = tuple[int, int]
 
 
+def check_range(y_range: range, x_range: range, beacons: list[tuple[Point, int]]) -> Point | None:
+    for y in y_range:
+        x = x_range.start
+        while x < x_range.stop:
+            overlap = max(radius - manhattan((x, y), beacon) + 1 for beacon, radius in beacons)
+            if overlap > 0:
+                x += overlap
+                continue
+            return x, y
+    return None
+
+
 def solve_puzzle(coords: list[tuple[Point, Point]]) -> None:
-    row = set()
-    tracked_row = int(sys.argv[1])
-    for scanner, beacon in coords:
-        fill_intersection(row, tracked_row, scanner, beacon)
-    for _, (x, y) in coords:
-        if y == tracked_row:
-            row.discard(x)
-    print(len(row))
+    beacons = list(map(lambda x: (x[0], manhattan(*x)), coords))
+    max_coord = 4_000_000 if len(sys.argv) == 1 else 20
+    checked_row = 2_000_000 if len(sys.argv) == 1 else 10
+    thread_count = cpu_count()
+    range_height = max_coord // thread_count
+    with ThreadPoolExecutor(thread_count) as executor:
+        futures = {
+            executor.submit(
+                check_range,
+                range(range_height * i, range_height * (i + 1)),
+                range(max_coord),
+                beacons,
+            )
+            for i in range(thread_count)
+        }
+        for future in as_completed(futures):
+            if result := future.result():
+                print(result[0] * 4_000_000 + result[1])
 
 
 def point(x: str, y: str) -> Point:
@@ -22,13 +46,6 @@ def point(x: str, y: str) -> Point:
 
 def manhattan(a: Point, b: Point) -> int:
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-
-def fill_intersection(row: set[int], tracked_row: int, scanner: Point, beacon: Point) -> None:
-    radius = manhattan(scanner, beacon)
-    width = radius - abs(scanner[1] - tracked_row)
-    for x in range(scanner[0] - width, scanner[0] + width + 1):
-        row.add(x)
 
 
 if __name__ == "__main__":
