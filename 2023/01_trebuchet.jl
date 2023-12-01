c72f7cd92ba93193e604cb75ba837bf4fd2eb1b4
technomunk <thegriffones@gmail.com>
2023-12-01 22:11:53 +0100

    Use pipe syntax

--- 2023/01_trebuchet.jl
+++ 2023/01_trebuchet.jl
@@ -2,15 +2,15 @@ NUMBER_RE = r"^(one|two|three|four|five|six|seven|eight|nine|\d)"
 DIGIT_RE = r"^\d"
 
 DIGITS = Base.ImmutableDict(
-    "one"=>"1",
-    "two"=>"2",
-    "three"=>"3",
-    "four"=>"4",
-    "five"=>"5",
-    "six"=>"6",
-    "seven"=>"7",
-    "eight"=>"8",
-    "nine"=>"9",
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9",
 )
 
 function digitize(digit)::String
@@ -21,7 +21,7 @@ function grab_first_last(line::String, re::Regex)::String
     first_digit = nothing
     last_digit = nothing
 
-    for i = 0:length(line)-1
+    for i = 0:length(line)
         m = match(re, chop(line, head=i, tail=0))
         if !isnothing(m)
             if isnothing(first_digit)
@@ -41,15 +41,21 @@ function grab_first_last(line::String, re::Regex)::String
     return "$first_digit$last_digit"
 end
 
+function parse_first_last_digit(line::String)::Int
+    return grab_first_last(line, DIGIT_RE) |> d -> parse(Int, d)
+end
+
+function parse_first_last_number(line::String)::Int
+    return grab_first_last(line, NUMBER_RE) |> d -> parse(Int, d)
+end
+
 function solve()
     lines = readlines()
-    digits = map((line) -> grab_first_last(line, DIGIT_RE), lines)
-    numbers = map((d) -> parse(Int, d), digits)
-    println(sum(numbers))
+    result = map(parse_first_last_digit, lines) |> sum
+    println(result)
 
-    digits = map((line) -> grab_first_last(line, NUMBER_RE), lines)
-    numbers = map((d) -> parse(Int, d), digits)
-    println(sum(numbers))
+    result = map(parse_first_last_number, lines) |> sum
+    println(result)
 end
 
 solve()
