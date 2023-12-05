b88914b28ed4bea2c6f7e19d6366b05dff6b28bc
Grigory Glukhov <grigory.glukhov@billogram.com>
2023-12-05 14:52:18 +0100

    Solve part 1, work on part 2

--- 2023/utils.jl
+++ 2023/utils.jl
@@ -18,3 +18,14 @@ end
 function _take(m::Nothing, s::AbstractString)
     m, s
 end
+
+function spliton(i::AbstractArray{T}, delim::T)::Vector{SubArray{T}} where {T}
+    result = []
+    prev_index = 1
+    for idx in findall(x -> x == delim, i)
+        push!(result, view(i, prev_index:idx - 1))
+        prev_index = idx + 1
+    end
+    push!(result, view(i, prev_index:length(i)))
+    return result
+end
