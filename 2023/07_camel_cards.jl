3e8da53e37daa58fe35ccb079b5a99b71af0fa23
Grigory Glukhov <grigory.glukhov@billogram.com>
2023-12-07 16:31:11 +0100

    Add a bug

--- 2023/07_camel_cards.jl
+++ 2023/07_camel_cards.jl
@@ -1,6 +1,6 @@
 function solve()
     plays = parse.(Play, readlines())
-    display(plays)
+    solve1(plays)
 end
 
 struct Hand
@@ -17,21 +17,106 @@ function parse(::Type{Play}, line::AbstractString)::Play
     Play(parse(Hand, hand), Base.parse(Int, bid))
 end
 function parse(::Type{Hand}, line::AbstractString)::Hand
-    Hand(reduce(((c, d) -> d[c] += 1; d), hand, Dict{Char,Int}()))
+    cards = Dict{Char,Int}()
+    for ch in line
+        cards[ch] = get(cards, ch, 0) + 1
+    end
+    Hand(cards)
 end
 
-function handtype(hand::Hand)::Int
-    if occursin("S", hand.cards)
-        return "S"
-    elseif occursin("H", hand.cards)
-        return "H"
-    elseif occursin("D", hand.cards)
-        return "D"
-    elseif occursin("C", hand.cards)
-        return "C"
-    else
-        return "N"
+function solve1(plays::AbstractArray{Play})
+    plays = sort(plays, by=p -> rank(p.hand) => sortstr(p.hand))
+    enumerate(plays) .|>
+        (p -> p[1]*p[2].bid) |>
+        sum |>
+        println
+end
+
+struct RankPair end
+struct RankTwoPair end
+struct RankThreeOfAKind end
+struct RankFullHouse end
+struct RankFourOfAKind end
+struct RankFiveOfAKind end
+
+function rank(hand::Hand)::Int
+    if is(hand, RankFiveOfAKind)
+        return 7
+    elseif is(hand, RankFourOfAKind)
+        return 6
+    elseif is(hand, RankFullHouse)
+        return 5
+    elseif is(hand, RankThreeOfAKind)
+        return 4
+    elseif is(hand, RankTwoPair)
+        return 3
+    elseif is(hand, RankPair)
+        return 2
     end
+    return 1
+end
+
+function is(hand::Hand, ::Type{RankFiveOfAKind})::Bool
+    for (card, count) in hand.cards
+        if count == 5
+            return true
+        end
+    end
+    false
+end
+function is(hand::Hand, ::Type{RankFourOfAKind})::Bool
+    for (card, count) in hand.cards
+        if count == 4
+            return true
+        end
+    end
+    false
+end
+function is(hand::Hand, ::Type{RankFullHouse})::Bool
+    is(hand, RankThreeOfAKind) && is(hand, RankPair)
+end
+function is(hand::Hand, ::Type{RankThreeOfAKind})::Bool
+    for (card, count) in hand.cards
+        if count == 3
+            return true
+        end
+    end
+    false
+end
+function is(hand::Hand, ::Type{RankTwoPair})::Bool
+    pairs = 0
+    for (card, count) in hand.cards
+        if count == 2
+            pairs += 1
+        end
+    end
+    pairs == 2
+end
+function is(hand::Hand, ::Type{RankPair})::Bool
+    for (card, count) in hand.cards
+        if count == 2
+            return true
+        end
+    end
+    false
+end
+
+const ORDER = "23456789TJQKA"
+
+function sortstr(hand::Hand)::String
+    result = ""
+    for (card, count) in sort!(collect(hand.cards), by=((p) -> p[2] => getindex(ORDER, p[1])), rev=true)
+        result *= repeat(card, count)
+    end
+    result
+end
+function getindex(str::AbstractString, ch::Char)::Int
+    for (i, c) in enumerate(str)
+        if c == ch
+            return i
+        end
+    end
+    error("not found")
 end
 
 solve()
