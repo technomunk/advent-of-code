7fdd3b65bd3c2ca664652323ffe63eb2c985fdce
technomunk <thegriffones@gmail.com>
2023-12-11 15:22:23 +0100

    Add pairs helper iterator

--- 2023/utils.jl
+++ 2023/utils.jl
@@ -29,3 +29,29 @@ function spliton(i::AbstractArray{T}, delim::T)::Vector{SubArray{T}} where {T}
     push!(result, view(i, prev_index:length(i)))
     return result
 end
+
+struct Pairs{T}
+    a::AbstractArray{T}
+end
+
+function Base.iterate(p::Pairs{T})::Union{Tuple{Tuple{T, T}, Tuple{Int, Int}}} where {T}
+    if length(p.a) <= 1
+        return nothing
+    end
+    return ((p.a[1], p.a[2]), (1, 3))
+end
+function Base.iterate(p::Pairs{T}, (a, b)::Tuple{Int, Int}) where {T}
+    if b > lastindex(p.a)
+        if a >= lastindex(p.a) - 1
+            return nothing
+        end
+        a += 1
+        b = a + 1
+    end
+    return ((p.a[a], p.a[b]), (a, b + 1))
+end
+
+function Base.length(p::Pairs{T})::Int where {T}
+    # triangle number
+    return div(length(p.a) * (length(p.a) - 1), 2)
+end
