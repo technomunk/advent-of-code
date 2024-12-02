43312b50824c1abe6ed80cec40409788817de1ca
technomunk <thegriffones@gmail.com>
2024-12-02 14:46:45 +0100

    Make day 1 work with abstracted boilerplate

--- 2024/01_historian_hysteria.zig
+++ 2024/01_historian_hysteria.zig
@@ -9,45 +9,43 @@ fn Solution(comptime T: type) type {
         right: std.ArrayList(T),
         counts: std.AutoHashMap(T, T),
 
-        fn init(allocator: anytype) Self {
+        pub fn init(allocator: anytype) Self {
             return Self{
-                .left = std.ArrayList.init(allocator),
-                .right = std.ArrayList.init(allocator),
-                .counts = std.AutoHashMap(i32, i32).init(allocator),
+                .left = std.ArrayList(T).init(allocator),
+                .right = std.ArrayList(T).init(allocator),
+                .counts = std.AutoHashMap(T, T).init(allocator),
             };
         }
 
-        fn deinit(self: *Self) void {
+        pub fn deinit(self: *Self) void {
             self.left.deinit();
             self.right.deinit();
             self.counts.deinit();
         }
 
-        fn processLine(self: *Self, line: []const u8) !void {
+        pub fn processLine(self: *Self, line: []const u8) !void {
             const spaceIndex = std.mem.indexOf(u8, line, "   ").?;
-            const l = try std.fmt.parseInt(i32, line[0..spaceIndex], 10);
-            const r = try std.fmt.parseInt(i32, line[spaceIndex + 3 ..], 10);
+            const l = try std.fmt.parseInt(T, line[0..spaceIndex], 10);
+            const r = try std.fmt.parseInt(T, line[spaceIndex + 3 ..], 10);
             try self.left.append(l);
             try self.right.append(r);
         }
 
-        fn solveP1(self: *Self) T {
+        pub fn solveP1(self: *Self) T {
             std.mem.sort(T, self.left.items, {}, std.sort.asc(T));
             std.mem.sort(T, self.right.items, {}, std.sort.asc(T));
             var total: T = 0;
             for (self.left.items, self.right.items) |l, r| {
-                var d = l - r;
-                if (d < 0) {
-                    d *= -1;
-                }
-                total += d;
+                total += @max(l, r) - @min(l, r);
             }
             return total;
         }
 
-        fn solveP2(self: *Self) !T {
+        pub fn solveP2(self: *Self) T {
             for (self.right.items) |r| {
-                const entry = try self.counts.getOrPutValue(r, 0);
+                const entry = self.counts.getOrPutValue(r, 0) catch {
+                    @panic("OOM");
+                };
                 entry.value_ptr.* += 1;
             }
             var result: T = 0;
