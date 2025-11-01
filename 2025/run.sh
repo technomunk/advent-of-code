a989d2dd61d587e626ca4aabfb8a1bab362c9e6d
technomunk <thegriffones@gmail.com>
2025-11-01 01:15:24 +0100

    Solve 2024 day 1 to double check my sanity

--- 2025/run.sh
+++ 2025/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-src_file="$(find . -name "$1*")"
-clang $src_file -O2 -o "solution"
+src_file="$(find . -name "$1_*")"
+clang $src_file lib/*.c -O2 -o "solution" -std=c11  -I. || exit
 ./solution
